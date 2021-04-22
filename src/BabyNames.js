a821a5a58a5f142254ed6e1cbbf89c3692f565f9
alinosratipour <alinosratipour@gmail.com>
2021-04-22 13:21:56 +0100

    fixed ESlint error

--- src/BabyNames.js
+++ src/BabyNames.js
@@ -1,34 +1,24 @@
+
+
 import React from "react";
 
-
 const BabyNames = (props) =>{
-
-
-
 return(
 
 <div className="container">
 <ul className="grid">
+
 {props.names.sort((a, b) => a.name.localeCompare(b.name)).map((item,index) =>{
     const {name,sex} = item;
-{if(sex=== "f"){
-return(
+    /* eslint-disable-next-line */
+       {if(sex=== "f"){
+        return <li key={index} className="girl">{name}</li>
+        } else if(sex === "m"){
+        return <li key={index} className="boy">{name}</li>      
+        }
+        }   
 
-<li key={index} className="girl">{name}</li>
-
- );
-} else if(sex === "m"){
- return <li key={index} className="boy">{name}</li>  
-}
-
-
-
-}
-
-   
-
-
- 
+ return true;
 })}
 </ul>
 </div>
